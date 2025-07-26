0400e0e676cf063ad90e3ca369784fdf28ced979
carrot <3033739510@qq.com>
2025-07-26 14:57:51 +0800

    mary的提交

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const obj = {
 const add = (a, b) => a + b;
 console.log(add(1, 2));
 
-const obj2={
+const obj2 = {
   name: "Mary",
-}
+};
+const count = (a, b) => a - b;
+console.log(count(5, 2));
